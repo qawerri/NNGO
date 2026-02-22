9d7b919a947e272f3e4fd656a5379fe59f8dc6a7
Svetlana Smirnova <sveta_smirniva@mail.ru>
2026-02-22 23:18:44 +0300

    Добавление авторизации

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "NNgo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "NNgo.Pages")]
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NNgo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
